16b55408f5c2296a62c08526577ce47ff8dc02e2
Debabrata <dev.566@gmail.com>
2017-12-24 03:14:34 -0600

    Initial files for Spring boot project

---
+++ src/main/java/com/my_devopps_project1/MyDevoppsProject1Application.java
@@ -0,0 +1,12 @@
+package com.my_devopps_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevoppsProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDevoppsProject1Application.class, args);
+	}
+}
